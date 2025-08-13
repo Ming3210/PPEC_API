8ec87c63031596ae20fe12d51547193ec20fdfa0
Duong <nguyendinhduong2005yt@gmail.com>
2025-08-13 10:25:25 +0700

    taọ entity

---
+++ src/main/java/com/ra/base_spring_boot/until/ExamStatus.java
@@ -0,0 +1,5 @@
+package com.ra.base_spring_boot.until;
+
+public enum ExamStatus {
+    UPCOMING, ONGOING, COMPLETED
+}
--- src/main/java/com/ra/base_spring_boot/until/TargetAudience.java
+++
@@ -1,5 +0,0 @@
-package com.ra.base_spring_boot.until;
-
-public enum TargetAudience {
-    BEGINNER, PROFESSIONAL, IT
-}
