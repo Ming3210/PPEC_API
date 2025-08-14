84be2dd51b34873d855790340c998620f83efc1c
mac <mac@172-11-136-25.lightspeed.dybhfl.sbcglobal.net>
2025-08-14 15:43:14 +0700

    feat: crud partner

---
+++ src/main/java/com/ra/base_spring_boot/dto/response/PaginationResponse.java
@@ -0,0 +1,14 @@
+package com.ra.base_spring_boot.dto.response;
+
+import com.ra.base_spring_boot.dto.request.PaginationDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class PaginationResponse<T> {
+    private List<T> items;
+    private PaginationDTO pagination;
+}
