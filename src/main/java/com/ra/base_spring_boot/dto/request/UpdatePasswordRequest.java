650df1deca5f03c98e3f24097f1deab9dfb96c83
unknown <vminh0227@gmail.com>
2025-08-20 03:04:14 +0700

    feat: change password

---
+++ src/main/java/com/ra/base_spring_boot/dto/request/UpdatePasswordRequest.java
@@ -0,0 +1,25 @@
+package com.ra.base_spring_boot.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdatePasswordRequest {
+
+    @NotBlank(message = "Mật khẩu cũ không được để trống")
+    private String oldPassword;
+
+    @NotBlank(message = "Mật khẩu mới không được để trống")
+    @Size(min = 8, max = 100, message = "Mật khẩu mới phải có ít nhất 6 ký tự và tối đa 100 ký tự")
+    private String newPassword;
+
+    @NotBlank(message = "Xác nhận mật khẩu không được để trống")
+    private String confirmPassword;
+}
